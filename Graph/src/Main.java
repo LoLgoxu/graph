440fc795dc6683d1b95223e87ec54bc22dbfd678
LoLgoxu <yijingye89@gmail.com>
2025-01-13 09:45:45 +0100

    changes in the ifs

--- Graph/src/Main.java
+++ Graph/src/Main.java
@@ -20,9 +20,9 @@ public class Main {
         int x, y;
 
         for(int i = 0; i < matrice.getMatriceSize()*matrice.getMatriceSize(); i++){ //ciclo for per eseguire gli input dei archi grandezza x grandezza per ottenere area della matrice
-            System.out.print("Inserire il numero della righa(INSERIRE -10): ");
+            System.out.print("Inserire il numero della righa(INSERIRE -10 PER TERMINARE): ");
             x = input.nextInt(); //input di x(riga)
-            System.out.print("Inserire il numero della colonna(INSERIRE -10): ");
+            System.out.print("Inserire il numero della colonna(INSERIRE -10 PER TERMINARE): ");
             y = input.nextInt(); //input di y(colonna)
 
             if ((x == -10 || y == -10)){ //if per uscire dal ciclo se verifica l'input di un numero negativo
@@ -31,16 +31,17 @@ public class Main {
             } else if (x >= 0 && y >= 0) { //else-if per convalidare se il numero inserito sia valido per la grandezza stabilita
                 if (x >= matriceSize || y >= matriceSize){
                     System.out.println("numeri inseriti invalidi");
+                } else {
+                    matrice.addArco(x, y);
+                    lista.addArco(x, y);
+                    lista.addVertice(x);
                 }
             }
-
-            matrice.addArco(x, y);
-            lista.addArco(x, y);
         }
 
-        for (int i = 0; i < matriceSize; i++){ // crea tutti i vertici in base alla grandezza della matrice
-            lista.addVertice(i);
-        }
+        /* for (int i = 0; i < matriceSize; i++){ crea tutti i vertici in base alla grandezza della matrice
+            lista.addVertice(x);
+        } */
         
         matrice.printMatrice();
         lista.printList();
